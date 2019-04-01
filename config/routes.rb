aafa39ef645de0a45de654bc004134892734a6a5
Edwardo <ejace1@gmail.com>
2019-04-01 19:28:40 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   
   resources :articles
   
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  # or:
+  # post 'users', to: 'users#create'
+  
 end
