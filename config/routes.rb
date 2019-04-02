6a81faab2e3f5b8da75f9b142eb4189ddb41a5be
Edwardo <ejace1@gmail.com>
2019-04-02 21:44:57 +0000

    Complete authentication

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
   # or:
   # post 'users', to: 'users#create'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  
 end
