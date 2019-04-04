e6ad2df0441340e28f67c8447e3d9e95d3efcd76
Edwardo <ejace1@gmail.com>
2019-04-04 02:27:04 +0000

    Add categories model and tests

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
+  resources :categories, except: [:destroy]
+  
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_02_233125) do
+ActiveRecord::Schema.define(version: 2019_04_03_025833) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "user_ide"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
